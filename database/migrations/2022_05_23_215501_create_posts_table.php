435516c83cff2eadbcd4db0d566ba1f0ad28dd97
mohamed <mohamed.mahmoud@camelcasetech.com>
2022-05-24 18:55:03 +0200

    remove slug column

--- database/migrations/2022_05_23_215501_create_posts_table.php
+++ database/migrations/2022_05_23_215501_create_posts_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->longText('content');
-            $table->string('slug');
             $table->timestamps();
         });
     }
