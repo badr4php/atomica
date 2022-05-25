c23cfbd61caff7a0afa72e88eefe83bead6033cf
mohamed <mohamed.mahmoud@camelcasetech.com>
2022-05-25 13:17:09 +0200

    create PostFactory, UserSeeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->text(50),
+            'content' => $this->faker->text(1000),
+        ];
+    }
+
+}
